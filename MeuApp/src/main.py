fb6219a73129a5a3411a33137edfb4c56582e90e
Mateus Rocha <mateus.barbosa09@aluno.ifce.edu.br>
2025-10-22 15:59:10 -0300

    Desenvolvimento 03: Tela de login que aceita um usuário específico

--- MeuApp/src/main.py
+++ MeuApp/src/main.py
@@ -1,15 +1,61 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.vertical_alignment = ft.VerticalAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.padding = 0
+
+    def tela_inicial():
+        page.appbar = ft.AppBar(
+            title=ft.Text("NUMERIFY"),
+            center_title=True,
+            bgcolor=ft.Colors.BLUE
+        )
+
+        page.navigation_bar = ft.NavigationBar(
+            destinations=[
+                ft.NavigationBarDestination(label="Início", icon=ft.Icons.HOME),
+                ft.NavigationBarDestination(label="Métodos", icon=ft.Icons.CONSTRUCTION),
+                ft.NavigationBarDestination(label="Perfil", icon=ft.Icons.PERSON)
+            ]
+        )
+
+        page.add(
+            ft.Text("HOME PAGE")
+        )
+
+        page.update()
+
+    def acesso_login(e):
+        if username.value == "admin" and password.value == "123":
+            page.controls.clear()
+            tela_inicial()
+
+            sucesso = ft.SnackBar(
+                content=ft.Text("Bem-Vindo ao Numerify"),
+                bgcolor=ft.Colors.GREEN,
+                show_close_icon=True,
+            )
+            page.overlay.append(sucesso)
+            sucesso.open = True
+            page.update()
+        else:
+            falho = ft.SnackBar(
+                content=ft.Text("Nome de usuário ou senha inválidos", color=ft.Colors.WHITE, size=20),
+                bgcolor=ft.Colors.RED,
+                show_close_icon=True,
+            )
+            page.overlay.append(falho)
+            falho.open = True
+            page.update()
+
 
     username = ft.TextField(
         label='Usuário',
         prefix_icon=ft.Icons.PERSON,
-        bgcolor=ft.Colors.BLACK,
+        bgcolor=ft.Colors.WHITE,
         border_radius=ft.border_radius.all(10),
-        border_color=ft.Colors.WHITE,
+        border_color=ft.Colors.BLUE,
         border_width=2,
         width=300,
     )
@@ -17,9 +63,9 @@ def main(page: ft.Page):
     password = ft.TextField(
         label="Senha",
         prefix_icon=ft.Icons.PERSON,
-        bgcolor=ft.Colors.BLACK,
+        bgcolor=ft.Colors.WHITE,
         border_radius=ft.border_radius.all(10),
-        border_color=ft.Colors.WHITE,
+        border_color=ft.Colors.BLUE,
         border_width=2,
         password=True,
         can_reveal_password=True,
@@ -31,6 +77,7 @@ def main(page: ft.Page):
         bgcolor=ft.Colors.BLUE,
         color=ft.Colors.WHITE,
         width=300,
+        on_click=acesso_login
     )
 
     page.add(
@@ -43,6 +90,5 @@ def main(page: ft.Page):
         )
     )
     page.update()
-
 
 ft.app(main)

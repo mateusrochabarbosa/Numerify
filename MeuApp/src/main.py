867d94f8ab8317a9278471d24739ccb58043b5fe
Mateus Rocha <mateus.barbosa09@aluno.ifce.edu.br>
2025-10-22 14:27:35 -0300

    Desenvolvimento 02: Esboço da interface de login

--- MeuApp/src/main.py
+++ MeuApp/src/main.py
@@ -5,16 +5,17 @@ def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     username = ft.TextField(
-        label='Username',
+        label='Usuário',
         prefix_icon=ft.Icons.PERSON,
         bgcolor=ft.Colors.BLACK,
         border_radius=ft.border_radius.all(10),
         border_color=ft.Colors.WHITE,
         border_width=2,
+        width=300,
     )
 
     password = ft.TextField(
-        label="Password",
+        label="Senha",
         prefix_icon=ft.Icons.PERSON,
         bgcolor=ft.Colors.BLACK,
         border_radius=ft.border_radius.all(10),
@@ -22,7 +23,26 @@ def main(page: ft.Page):
         border_width=2,
         password=True,
         can_reveal_password=True,
+        width=300
     )
+
+    button_login = ft.ElevatedButton(
+        text="Entrar",
+        bgcolor=ft.Colors.BLUE,
+        color=ft.Colors.WHITE,
+        width=300,
+    )
+
+    page.add(
+        ft.Container(
+            content=ft.Column(
+                controls=[username, password, button_login],
+                alignment=ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER
+            )
+        )
+    )
+    page.update()
 
 
 ft.app(main)
